40f1303dbf129d7222fa0ab2a3279131be7f49ca
emill3000 <emill3000@yahoo.com>
2016-03-07 22:03:23 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
+
+fido = Dog.new
+snoopy = Dog.new
+
+fido.name = "Fido"
+snoopy.name = "Snoopy"
+snoopy.breed = "Beagle"
